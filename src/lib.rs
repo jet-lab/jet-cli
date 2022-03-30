0e861159ae7a4f0876ee98f36a46de62e7ed06d1
Matthew Callens <callensmatt@gmail.com>
2022-03-30 18:29:58 -0400

    add utility macros with usage comments and close_account for staking

--- src/lib.rs
+++ src/lib.rs
@@ -39,8 +39,14 @@ pub fn run(opts: Opts) -> Result<()> {
 
 /// Macro to handle the instantiation of a program client and
 /// the designating signer keypair for the argued config and program ID.
+///
+/// # Example
+///
+/// ```
+/// let program = program_client!(config, jet_staking::ID);
+/// ```
 macro_rules! program_client {
-    ($config: ident, $program: expr) => {{
+    ($config:ident, $program:expr) => {{
         let payer = Rc::new($config.keypair);
         (
             Client::new_with_options(
@@ -54,3 +60,58 @@ macro_rules! program_client {
     }};
 }
 pub(crate) use program_client;
+
+/// Macro to create and wrap transaction calls with automatic error
+/// transformation and logging of the confirmation signature.
+///
+/// # Example
+///
+/// Basic example without an instruction arguments.
+///
+/// ```
+/// send_and_log!(
+///     program,
+///     my_program::accounts::Init {
+///         authority: program.payer(),
+///         system_program: system_program::ID,
+///     },
+///     signer
+/// );
+/// ```
+///
+/// You can also provide instruction arguments as a third parameter.
+///
+/// ```
+/// send_and_log!(
+///     program,
+///     my_program::accounts::Init {
+///         authority: program.payer(),
+///         system_program: system_program::ID,
+///     },
+///     my_program::instruction::Init { value: 25 },
+///     signer
+/// );
+/// ```
+macro_rules! send_and_log {
+    ($program:ident, $accs:expr, $signer:ident) => {{
+        $program
+            .request()
+            .accounts($accs)
+            .signer($signer.as_ref())
+            .send()
+            .map(|sig| println!("Signature: {}", sig))
+            .map_err(Into::into)
+    }};
+
+    ($program:ident, $accs:expr, $args:expr, $signer:ident) => {{
+        $program
+            .request()
+            .accounts($accs)
+            .args($args)
+            .signer($signer.as_ref())
+            .send()
+            .map(|sig| println!("Signature: {}", sig))
+            .map_err(Into::into)
+    }};
+}
+pub(crate) use send_and_log;
--- src/staking.rs
+++ src/staking.rs
@@ -7,28 +7,26 @@ use anchor_spl::token;
 use anyhow::Result;
 use clap::Subcommand;
 use jet_staking::accounts;
-use jet_staking::instruction;
+use jet_staking::instruction as args;
 use jet_staking::state::StakePool;
 use std::rc::Rc;
 
 use crate::config::ConfigOverride;
-use crate::program_client;
+use crate::{program_client, send_and_log};
 
 /// Staking program based subcommand enum variants.
 #[derive(Debug, Subcommand)]
 pub enum Command {
+    #[clap(about = "Deposit to a stake pool from your account")]
     Add {
         amount: Option<u64>,
         pool: Pubkey,
         token_account: Pubkey,
     },
-    CreateAccount {
-        pool: Pubkey,
-    },
-    Unbond {
-        amount: Option<u64>,
-        pool: Pubkey,
-    },
+    #[clap(about = "Close your staking account")]
+    CloseAccount { pool: Pubkey },
+    #[clap(about = "Create a new staking account")]
+    CreateAccount { pool: Pubkey },
 }
 
 /// The main entry point and handler for all staking
@@ -40,8 +38,8 @@ pub fn entry(cfg: &ConfigOverride, subcmd: &Command) -> Result<()> {
             pool,
             token_account,
         } => add_stake(cfg, pool, token_account, amount),
+        Command::CloseAccount { pool } => close_account(cfg, pool),
         Command::CreateAccount { pool } => create_account(cfg, pool),
-        Command::Unbond { amount, pool } => unbond_stake(cfg, pool, amount),
     }
 }
 
@@ -63,23 +61,39 @@ fn add_stake(
         stake_pool_vault, ..
     } = program.account(*pool)?;
 
-    let sig = program
-        .request()
-        .accounts(accounts::AddStake {
+    send_and_log!(
+        program,
+        accounts::AddStake {
             stake_pool: *pool,
             stake_pool_vault,
             stake_account,
             payer: program.payer(),
             payer_token_account: *token_account,
             token_program: token::ID,
-        })
-        .args(instruction::AddStake { amount: *amount })
-        .signer(signer.as_ref())
-        .send()?;
+        },
+        args::AddStake { amount: *amount },
+        signer
+    )
+}
 
-    println!("Signature: {}", sig);
+/// The function handler for the staking subcommand that allows user to
+/// close their staking account and receive the rent funds back.
+fn close_account(overrides: &ConfigOverride, pool: &Pubkey) -> Result<()> {
+    let config = overrides.transform()?;
 
-    Ok(())
+    let stake_account = find_staking_address(pool, &config.keypair.pubkey());
+
+    let (program, signer) = program_client!(config, jet_staking::ID);
+
+    send_and_log!(
+        program,
+        accounts::CloseStakeAccount {
+            owner: program.payer(),
+            closer: program.payer(),
+            stake_account,
+        },
+        signer
+    )
 }
 
 /// The function handler for the staking subcommand that allows users to create a
@@ -92,73 +106,26 @@ fn create_account(overrides: &ConfigOverride, pool: &Pubkey) -> Result<()> {
 
     let (program, signer) = program_client!(config, jet_staking::ID);
 
-    let sig = program
-        .request()
-        .accounts(accounts::InitStakeAccount {
+    send_and_log!(
+        program,
+        accounts::InitStakeAccount {
             owner: program.payer(),
             auth,
             stake_pool: *pool,
             stake_account,
             payer: program.payer(),
             system_program: system_program::ID,
-        })
-        .signer(signer.as_ref())
-        .send()?;
-
-    println!("Signature: {}", sig);
-
-    Ok(())
+        },
+        signer
+    )
 }
 
-/// The function handler for the staking subcommand that allows users to unbond
-/// existing staking from the pool back into their account.
-fn unbond_stake(overrides: &ConfigOverride, pool: &Pubkey, amount: &Option<u64>) -> Result<()> {
-    let config = overrides.transform()?;
-
-    let stake_account = find_staking_address(pool, &config.keypair.pubkey());
-    let unbonding_account = find_unbonding_address(&stake_account, 0); // FIXME:
-
-    let (program, signer) = program_client!(config, jet_staking::ID);
-
-    let StakePool {
-        stake_pool_vault, ..
-    } = program.account(*pool)?;
-
-    let sig = program
-        .request()
-        .accounts(accounts::UnbondStake {
-            owner: program.payer(),
-            payer: program.payer(),
-            stake_account,
-            stake_pool: *pool,
-            stake_pool_vault,
-            unbonding_account,
-            system_program: system_program::ID,
-        })
-        .args(instruction::UnbondStake {
-            seed: 0, // FIXME:,
-            amount: *amount,
-        })
-        .signer(signer.as_ref())
-        .send()?;
-
-    println!("Signature: {}", sig);
-
-    Ok(())
-}
-
+/// Derive the public key of a `jet_auth::UserAuthentication` program account.
 fn find_auth_address(owner: &Pubkey) -> Pubkey {
-    Pubkey::find_program_address(&[owner.as_ref()], &jet_staking::ID).0
+    Pubkey::find_program_address(&[owner.as_ref()], &jet_auth::ID).0
 }
 
+/// Derive the public key of a `jet_staking::StakeAccount` program account.
 fn find_staking_address(stake_pool: &Pubkey, owner: &Pubkey) -> Pubkey {
     Pubkey::find_program_address(&[stake_pool.as_ref(), owner.as_ref()], &jet_staking::ID).0
-}
-
-fn find_unbonding_address(stake_account: &Pubkey, seed: u32) -> Pubkey {
-    Pubkey::find_program_address(
-        &[stake_account.as_ref(), seed.to_le_bytes().as_ref()],
-        &jet_staking::ID,
-    )
-    .0
 }
